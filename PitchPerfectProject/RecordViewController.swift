4cf60326637de72a67a146fc4560a49ffaeab24e
Jason Malia <jmalia3@gatech.edu>
2016-07-09 17:32:52 -0500

    Add Imagesets

--- PitchPerfectProject/RecordViewController.swift
+++ PitchPerfectProject/RecordViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class RecordViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    enum recordViewState {
+        case READY_TO_RECORD
+        case RECORDING
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("Recording audio...")
+        setViewState(.RECORDING)
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("Stopped recording.")
+        setViewState(.READY_TO_RECORD)
+        //performSegueWithIdentifier("playbackSeque", sender: )
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        setViewState(.READY_TO_RECORD)
+    }
+    
+    func setViewState(state: recordViewState){
+        // Set up state of buttons/label
+        switch state {
+            
+        case .READY_TO_RECORD:
+            recordButton.enabled = true
+            recordButton.hidden = false
+            stopButton.enabled = false
+            stopButton.hidden = true
+            recordingLabel.text = "Record Audio"
+            
+        case .RECORDING:
+            recordButton.enabled = false
+            recordButton.hidden = true
+            stopButton.enabled = true
+            stopButton.hidden = false
+            recordingLabel.text = "Stop Recording"
+            
+        }
+    }
 
 }
 
